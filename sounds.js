1e961b5f1a8930346779ccf01fc54afa69432386
Alex Kumpula <alex.kumpula01@gmail.com>
2026-08-15 05:07:52 -0600

    Reorder and relabel sounds

--- sounds.js
+++ sounds.js
@@ -1,71 +1,71 @@
 // Add sounds using only a custom name and a YouTube link.
 window.SOUNDBOARD_SOUNDS = [
   {
-    name: "HOH Competition",
-    url: "https://www.youtube.com/watch?v=NTXJpYysvNQ",
-  },
-  {
-    name: "Eviction Sting",
-    url: "https://www.youtube.com/watch?v=1uIe1PpaUdA",
-  },
-  {
-    name: "Next On…",
-    url: "https://www.youtube.com/watch?v=wDiw5eN9138",
-  },
-  {
-    name: "Theme Song",
+    name: "Intermission Music (BB Theme Song)",
     url: "https://www.youtube.com/watch?v=58UXX7yUicI",
   },
   {
-    name: "HOH Victory",
-    url: "https://www.youtube.com/watch?v=lTJsNufIE5s",
-  },
-  {
-    name: "Celebration Classic",
-    url: "https://www.youtube.com/watch?v=bHEgq_ibklo",
-  },
-  {
-    name: "Nomination Ceremony",
-    url: "https://www.youtube.com/watch?v=fa1FTbzgRlA",
-  },
-  {
-    name: "Bring It",
-    url: "https://www.youtube.com/watch?v=LhRH4Mtb5wQ",
-  },
-  {
-    name: "Post-Competition",
-    url: "https://www.youtube.com/watch?v=bUJpATUcS0w",
-  },
-  {
-    name: "Celebration Song",
-    url: "https://www.youtube.com/watch?v=4LGvg0GCHWg",
-  },
-  {
-    name: "HOH Win - Modern",
-    url: "https://www.youtube.com/watch?v=qXKZSFs7O6s",
-  },
-  {
-    name: "Veto Music",
-    url: "https://www.youtube.com/watch?v=u7Mzm_JExw8",
-  },
-  {
-    name: "Ending Music",
-    url: "https://www.youtube.com/watch?v=1jd7cJqCMAU",
-  },
-  {
-    name: "Gold Record",
+    name: "Intermission Music (Dramatic)",
     url: "https://www.youtube.com/watch?v=MUm5jZ_6qr8",
   },
   {
-    name: "Live Vote - V2",
-    url: "https://www.youtube.com/watch?v=ZnTSx0ekGD0",
+    name: "Post-Competition Theme",
+    url: "https://www.youtube.com/watch?v=bUJpATUcS0w",
+  },
+  {
+    name: "HOH Competition Theme",
+    url: "https://www.youtube.com/watch?v=NTXJpYysvNQ",
+  },
+  {
+    name: "Nomination Ceremony",
+    url: "https://www.youtube.com/watch?v=wDiw5eN9138",
+  },
+  {
+    name: "Nomination Ceremony - Alternate",
+    url: "https://www.youtube.com/watch?v=fa1FTbzgRlA",
+  },
+  {
+    name: "Veto Competition Theme",
+    url: "https://www.youtube.com/watch?v=LhRH4Mtb5wQ",
+  },
+  {
+    name: "Veto Meeting Theme",
+    url: "https://www.youtube.com/watch?v=u7Mzm_JExw8",
   },
   {
     name: "Live Vote",
     url: "https://www.youtube.com/watch?v=pRyJL8AIHFg",
   },
   {
+    name: "Live Vote - Alternate",
+    url: "https://www.youtube.com/watch?v=ZnTSx0ekGD0",
+  },
+  {
+    name: "Eviction Sting",
+    url: "https://www.youtube.com/watch?v=1uIe1PpaUdA",
+  },
+  {
+    name: "Ending Of Day Music",
+    url: "https://www.youtube.com/watch?v=1jd7cJqCMAU",
+  },
+  {
     name: "Mastermind Intro Speech",
     url: "https://youtu.be/NtoD4k1x5hQ",
+  },
+  {
+    name: "(UNUSED) Celebration Classic",
+    url: "https://www.youtube.com/watch?v=bHEgq_ibklo",
+  },
+  {
+    name: "(UNUSED) Celebration Song",
+    url: "https://www.youtube.com/watch?v=4LGvg0GCHWg",
+  },
+  {
+    name: "(UNUSED) HOH Victory",
+    url: "https://www.youtube.com/watch?v=lTJsNufIE5s",
+  },
+  {
+    name: "(UNUSED) HOH Win - Modern",
+    url: "https://www.youtube.com/watch?v=qXKZSFs7O6s",
   },
 ];
